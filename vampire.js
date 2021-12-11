4cb6f4f85e46c497b4f39f03c8f51c7204a2cef4
Val Hustle <stars@MacBook-Air.local>
2021-12-10 22:50:56 -0500

    adding vampire as last commit failed double checking

--- vampire.js
+++ vampire.js
@@ -10,22 +10,37 @@ class Vampire {
 
   // Adds the vampire as an offspring of this vampire
   addOffspring(vampire) {
-
+    vampire.creator = this;
+    this.offspring.push(vampire);
+    //vampire.creator.offspring.push(vampire);
   }
 
   // Returns the total number of vampires created by that vampire
   get numberOfOffspring() {
-
+    return this.offspring.length;
   }
 
   // Returns the number of vampires away from the original vampire this vampire is
   get numberOfVampiresFromOriginal() {
-
+    let numberofVamp=0;
+    let currentVampire = this;
+    while (currentVampire.creator){
+      currentVampire = currentVampire.creator;
+      numberofVamp++;
+    }
+    return numberofVamp;
   }
 
   // Returns true if this vampire is more senior than the other vampire. (Who is closer to the original vampire)
-  isMoreSeniorThan(vampire) {
+  isMoreSeniorThan(vampire) { //needs to check who is closer to the original vampire
+    //rootVampire.isMoreSeniorThan(offspring1)
+    return this.numberOfVampiresFromOriginal < vampire.numberOfVampiresFromOriginal; //calls the getter
+  }
 
+  // Returns the vampire object with that name, or null if no vampire exists with that name
+  vampireWithName(name) {
+  if (this.name){ return this}
+  else{ return null}
   }
 
   /** Stretch **/
